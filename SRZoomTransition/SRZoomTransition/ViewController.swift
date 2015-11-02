e156a71054c900652350610c145506269af97ffe
Louis Tur <louis.tur@gmail.com>
2015-11-01 21:59:51 -0500

    Adds carthage, correctly links frameworks and dsym, adds framework and header search paths to project

---
+++ SRZoomTransition/SRZoomTransition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SRZoomTransition
+//
+//  Created by Louis Tur on 11/1/15.
+//  Copyright © 2015 Louis Tur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
